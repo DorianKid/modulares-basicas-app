989b593cefedcf980ac2cc4e8b7749f5e4c0a65b
Dario Niño <130793949+DorianKid@users.noreply.github.com>
2025-09-02 12:43:18 -0600

    Update build_index.py

--- app/utils/build_index.py
+++ app/utils/build_index.py
@@ -1,6 +1,6 @@
 import os, glob
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain_community.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 
